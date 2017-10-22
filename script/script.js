974238b0f928da21de39ded9da17fbc2a828d033
Satsarof <32930912+Satsarof@users.noreply.github.com>
2017-10-22 03:59:56 +0200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,5 +1,10 @@
+//Variables globales:
 var deck = []; //Array con las posiciones de las cartas
-
+var flippedCards = 0;
+var firstCard;
+var aciertos = 0;
+var timer;
+var interval;
 //Metodos:
 $(document).ready(function(){
 	//Funciones de estilo:
@@ -13,10 +18,16 @@ $(document).ready(function(){
 	//Función de inicio de juego:
 	$("#play").click(function(){
 		var N = $("#images").val();
-		var timer = $("#timer").val();
-		//Removemos imagenes de anteriores partidas:
+		timer = $("#timer").val();
+		$("#play").text("Reiniciar");
+		//Removemos imagenes y valores de anteriores partidas:
 		$( "img" ).remove();
 		$( "br" ).remove();
+		$( ".aciertos" ).remove();
+		$( ".par" ).remove();
+		$( ".time" ).remove();
+		$( ".timer" ).remove();
+		
 		//Comprobamos que el tiempo esta en los parametros establecidos:
 		if(timer < 10 || timer > 120){
 			alert("Introduce un tiempo entre 10 y 120 segundos");
@@ -36,7 +47,8 @@ $(document).ready(function(){
 		x = 0;
 		$("body").append('<br>');
 		while ( x < N ){
-			var str = '<img id="' + x.toString() + '"src="images/back.png" alt="fondo" state value="0">'
+			y = (Number(N) + Number(x));
+			var str = '<img id="' + y.toString() + '"src="images/back.png" alt="fondo" state value="0">'
 			$("body").append(str);
 			deck[deck.length]=x;
 			x++;
@@ -54,6 +66,11 @@ $(document).ready(function(){
 			deck[indiceActual] = deck[indiceAleatorio];
 			deck[indiceAleatorio] = valorTemporal;
 		}
+		aciertos = 0;
+		$("body").append('<p class="par">Aciertos: <input type="text" class = "aciertos" value="0"  readonly></p>');
+		//Activamos el reloj:
+		$("body").append('<p class="time">Temporizador: <input type="text" class = "timer" value="' + timer.toString() + '" readonly></p>');
+		timerControl(N);
 	});
 
 	//Función de volteo de carta:
@@ -62,8 +79,70 @@ $(document).ready(function(){
 			return;
 		}
 		$(this).attr("value", "1");
-		
-		
+		var imgNumber = Number( $(this).attr('id') );
+		var str = 'images/' + deck[imgNumber].toString() + '.png';
+		//Animación de cambio de carta:
+		$(this).hide();
+		$(this).attr("src", str);
+		$(this).show();
+		//Sumamos contadores:
+		flippedCards++;
+		//Depende de las cartas levantadas realizaremos una acción u otra:
+		if(flippedCards == 1){
+			firstCard = $(this).attr('id');
+		}
+		var c1,c2;
+		if(flippedCards == 2){
+			c1 = deck[(Number(firstCard))];
+			c2 = deck[Number($(this).attr('id'))];
+			//Caso de las cartas son iguales:
+			if(c1 == c2){
+				aciertos++;
+				$(".aciertos").attr('value',aciertos);
+				flippedCards = 0;
+				var imagenes = $("#images").val();
+				if( imagenes == aciertos ) {
+					setTimeout(function(){alert("Enhorabuena, has ganado la partida");},500);
+					clearInterval(interval);
+				}
+			}
+			//Caso de las cartas no son iguales:
+			else{
+				var id = $(this).attr('id');
+				setTimeout(function(){flipBack(id,firstCard);},200);	
+				flippedCards = 0;
+				$(this).attr('value',"0");
+				$("#"+firstCard).attr('value',"0");
+			}
+		}
 	});
 });
-	
+//Función para volver la carta a la parte trasera:
+function flipBack(id,id2) {
+	var str = 'images/back.png'
+	$("#"+id).hide();
+	$("#"+id).attr("src", str);
+	$("#"+id).show();
+	$("#"+id2).hide();
+	$("#"+id2).attr("src", str);
+	$("#"+id2).show();
+}
+//Función para controlar el temporizador:
+function timerControl(cardNumber){
+	interval = setInterval(function(){
+		timer--;
+		$(".timer").attr('value',timer);
+		if(timer == -1 ){
+			$(".timer").attr('value',0);
+			clearInterval(interval);
+			$(".timer").attr('value',0);
+			var x = 0;
+			while (x < cardNumber*2){
+				$("#"+x).attr("value", "1");
+				x++;
+			}
+			alert("Has perdido, prueba a intentarlo de nuevo");
+		}
+	}, 1000);
+
+}
